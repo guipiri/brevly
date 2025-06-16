ed579410fcaf4b6c42ed9ba018bf763a84940b21
guipiri <gui.soliveiras@gmail.com>
2025-06-16 19:50:57 -0300

    style: create short link form styles

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,5 +1,73 @@
+import { WarningIcon } from '@phosphor-icons/react'
+
 function App() {
-  return <h1 className="text-xs">Hello world!</h1>
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
+  return (
+    <div className="bg-gray-200 h-dvh flex flex-col items-center p-2">
+      <div className="p-8 max-w-[22.5rem] w-full bg-white rounded-sm flex flex-col gap-5">
+        <h2 className="text-lg text-gray-600">Novo link</h2>
+        <form
+          onSubmit={onSubmit}
+          onInvalid={(e) => {
+            e.preventDefault()
+            console.log(e)
+          }}
+          id="createLink"
+          className="flex flex-col gap-4"
+        >
+          <div className="w-full flex flex-col gap-2 outline-">
+            <label className="text-xs uppercase" htmlFor="url">
+              Link Original
+            </label>
+            <input
+              className="px-4 rounded-sm text-md text-gray-600 font-sans font-normal ring-2 ring-gray-300 outline-none h-12 focus:ring-blue-base error:ring-danger user-invalid:ring-danger"
+              name="url"
+              id="url"
+              type="url"
+              required
+              placeholder="www.exemplo.com.br"
+            />
+            <div className="text-sm text-gray-500 flex gap-2 items-center">
+              <WarningIcon width={14} className="fill-danger" />
+              <span>Insira uma url válida.</span>
+            </div>
+          </div>
+          <div className="w-full flex flex-col gap-2 outline-">
+            <label className="text-xs uppercase" htmlFor="alias">
+              Link Encurtado
+            </label>
+            <div className="has-user-invalid:ring-danger has-focus:ring-blue-base ring-2 flex items-center px-4 rounded-sm text-gray-400 text-md font-sans font-normal ring-gray-300 h-12">
+              brev.ly/
+              <input
+                className="outline-none h-full w-full text-md text-gray-600 font-normal"
+                name="alias"
+                id="alias"
+                type="text"
+                required
+                pattern="^[a-zA-Z0-9&_-]+$"
+              />
+            </div>
+            <div className="text-sm text-gray-500 flex gap-2 items-center">
+              <WarningIcon width={14} className="fill-danger" />
+              <span>
+                Insira uma url minúscula e sem espaço/caracter especial.
+              </span>
+            </div>
+          </div>
+        </form>
+        <button
+          type="submit"
+          form="createLink"
+          disabled={false}
+          className="w-full bg-blue-base h-12 px-5 rounded-sm text-md text-white disabled:opacity-50 hover:bg-blue-dark hover:cursor-pointer transition-all"
+        >
+          Salvar Link
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default App
