6056b2eb73ba184ea298c01996b5d196371fafa5
guipiri <gui.soliveiras@gmail.com>
2025-06-23 16:17:44 -0300

    style: responsivity in list links with mobile with low height

--- web/src/components/home-header.tsx
+++ web/src/components/home-header.tsx
@@ -2,7 +2,7 @@ import logo from '../assets/Logo.svg'
 
 function HomeHeader() {
   return (
-    <header className="pb-6 pt-8 w-full bg-gray-200 sm:max-w-[60.5rem] mx-auto">
+    <header className="pb-4 sm:pb-6 sm:pt-8 w-full bg-gray-200 sm:max-w-[60.5rem] mx-auto">
       <img className="block m-auto sm:ml-0" src={logo} alt="logo-brevly" />
     </header>
   )
--- web/src/global-layout.tsx
+++ web/src/global-layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router'
 
 function GlobalLayout() {
   return (
-    <div className="p-2 bg-gray-200 h-dvh pt-8 flex flex-col">
+    <div className="p-2 pt-4 bg-gray-200 h-dvh sm:pt-8 flex flex-col">
       <Outlet />
     </div>
   )
